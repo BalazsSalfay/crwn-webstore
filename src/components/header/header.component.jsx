c96d3651aa2c4819aecc41a58db498fd29bf209b
Balázs Salfay <balazs.salfay@gmail.com>
2020-03-18 16:59:30 +0100

    cart icon and component added with the toogle feature

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,8 +6,10 @@ import { auth } from './../../firebase/firebase.utils';
 import { connect } from 'react-redux';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to="/" >
       <Logo className='logo' />
@@ -20,20 +22,26 @@ const Header = ({ currentUser }) => (
         CONTACT
       </Link>
       {currentUser ? (
-          <div className='option' onClick={() => auth.signOut()} >
-            SIGN OUT
-          </div> 
-        ) : (
+        <div className='option' onClick={() => auth.signOut()} >
+          SIGN OUT
+        </div>
+      ) : (
           <Link className='option' to="/signin">
-           SIGN IN
-          </Link> 
-        )}   
+            SIGN IN
+          </Link>
+        )}
+      <CartIcon />
     </div>
+    {
+      hidden ? null :
+        <CartDropdown />
+    }
   </div>
 );
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
 })
 
 export default connect(mapStateToProps)(Header);
