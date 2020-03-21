97028f0eaba9b28b6dd195d67b7f68215cc44885
Balázs Salfay <balazs.salfay@gmail.com>
2020-03-21 20:34:13 +0100

    collection get from firestore with redux

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -54,6 +54,24 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
   return await batch.commit();
 }
 
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    }
+  });
+
+  transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collections.title.toLowerCase()] = collection;
+    return accumulator;
+  } , {});
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
